45f2fa3ebbfb1306ae109ff5da608325284d1575
fazil <muhammedfazil104@gmail.com>
2023-07-13 00:06:55 +0530

    Introduction partial done

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,29 +1,46 @@
-import Link from 'next/link'
-import React from 'react'
-import styles from '../styles/NavBar.module.css'
+import Link from "next/link";
+import React from "react";
+import styles from "../styles/NavBar.module.css";
+import { Link as ScrollLink} from "react-scroll";
 //INtroduction
-//skills 
+//skills
 //Contacts
+const SmoothScrollLink = ({ to, children }) => {
+
+  return (
+    <ScrollLink
+      to={to}
+      smooth={true}
+      duration={500}
+      className={styles["nav-link"]}
+      activeClass={styles["active-link"]}
+    >
+      {children}
+    </ScrollLink>
+  );
+};
 
 const NavBar = () => {
   return (
     <nav className={styles["navbar-container"]}>
-      <div className={styles["logo"]}><Link href='#'>Fazil</Link></div>
+      <div className={styles["logo"]}>
+        <Link href="#">Fazil</Link>
+      </div>
       <div className={styles["nav-links"]}>
         <ul>
-            <li>
-                <Link href='#'>About me</Link>
-            </li>
-            <li>
-                <Link href='#'>Skills</Link>
-            </li>
-            <li>
-                <Link href='#'>Contact</Link>
-            </li>
+          <li>
+            <SmoothScrollLink to="introduction">About me</SmoothScrollLink>
+          </li>
+          <li>
+            <SmoothScrollLink to="skills">Skills</SmoothScrollLink>
+          </li>
+          <li>
+            <SmoothScrollLink to="contact">Contact</SmoothScrollLink>
+          </li>
         </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import styles from "@/styles/Main.module.css";
 import NavBar from "@/components/NavBar";
-
+import Introduction from "@/components/Introduction";
 
 export default function Home() {
   return (
     <>
       <div className={styles["main-container"]}>
         <NavBar />
+        <div className={styles["section"]} id="introduction">
+          <Introduction/>
+        </div>
+        <div className={styles["section"]} id="skills">
+          Skills
+        </div>
+        <div className={styles["section"]} id="contact">
+          Contact
+        </div>
+
       </div>
     </>
   );
