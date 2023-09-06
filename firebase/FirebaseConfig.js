3b896efb9bc920a06ea0e49df1373dd08952fa0f
fazil <muhammedfazil104@gmail.com>
2023-09-07 00:41:38 +0530

    Securing API keys

--- firebase/FirebaseConfig.js
+++ firebase/FirebaseConfig.js
@@ -6,12 +6,12 @@
 
 ***REMOVED***
 ***REMOVED***
-  apiKey: "AIzaSyDXUYvJpoKK78_noHMHnyVTwIVzZYemyn4",
-  authDomain: "sample-nextjs-project.firebaseapp.com",
-  projectId: "sample-nextjs-project",
-  storageBucket: "sample-nextjs-project.appspot.com",
-  messagingSenderId: "642775831052",
-  appId: "1:642775831052:web:9f7f3dba5b20c6ab735429"
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
 ***REMOVED***
 
 ***REMOVED***
